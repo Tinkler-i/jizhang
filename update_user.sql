9ef84fdd1fe6d18775a85865de11481a573f5094
gongy <gongyong0502@outlook.com>
2026-01-08 20:15:51 +0800

    vue基础以及清理遗留的前端代码

--- update_user.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM user WHERE username='admin';
-INSERT INTO user (username, password, status) VALUES ('admin', '$2a$10$PEeFKpB9LdJddx5LJ8Tz5OduqWvyNKHWnqmDMl9F6fL2HvO3q0h4C', 1);
-SELECT * FROM user WHERE username='admin';
