b609d143f49eb537a1d04a0e8e48a81e4dcc5a3f
gongy <gongyong0502@outlook.com>
2026-02-12 20:15:28 +0800

    允许外部访问

--- src/main/resources/static/app/vite.config.js
+++ src/main/resources/static/app/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173,
+    host: '0.0.0.0',// 允许外部访问
     proxy: {
       '/jizhang/api': {
         target: 'http://localhost:8080',
